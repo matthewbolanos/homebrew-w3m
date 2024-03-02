0d224536cb796ef9ddbcd4f5f8a58586261280d1
Matthew Bolanos <matthewbolanos@gmail.com>
2024-03-02 00:40:37 +0000

    Add w3m formula

--- w3m.rb
+++ w3m.rb
@@ -27,6 +27,18 @@ class W3m < Formula
       regex(/href=.*?w3m[._-]v?(\d+(?:\.\d+)+)\.orig\.t/i)
     end
   
+    bottle do
+      sha256 arm64_sonoma:   "efae67d8d635d8f05a27fc9ae4e75156bffa465828735428e3dfb6d1a117b6eb"
+      sha256 arm64_ventura:  "fc4a77c30411f61b24a69be7ac380d6f79d3e9617c47f18f9c26e9c7a5ae11ef"
+      sha256 arm64_monterey: "f987092472928a6f55bc65930ca911de4415f312cf9c9b8f3662baf4058b4b05"
+      sha256 arm64_big_sur:  "d777d1b1193a49785df6150d908e38db8b2de415432f4acc55a635be32e69f64"
+      sha256 sonoma:         "6a3667e99c6b8a5a0febbbe8567ed3a6a712d8421e34176cc2a51c7e20019fd0"
+      sha256 ventura:        "9403514e48aabc3e5ed768524465eafa7bb5b5f1f67f3a128fe98a1fbae4aaa8"
+      sha256 monterey:       "9e6a1fc7660ebab1bce04646cc625d107b43e0a5cba52c5b1f9868f56b4e4825"
+      sha256 big_sur:        "3e32fcd2f971f88a8dcac24702147ff5847afb329d9c54cadd40e9c102bcb3c5"
+      sha256 x86_64_linux:   "1835ec7faed90c796e7290a5b6271dda1ac6b2bdb15ce577367852ad92681c39"
+    end
+  
     depends_on "pkg-config" => :build
     depends_on "bdw-gc"
     depends_on "openssl@3"
@@ -40,12 +52,11 @@ class W3m < Formula
     end
   
     def install
-      # Work around configure issues with Xcode 12
-      ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
+      # Fix compile with newer Clang
+      ENV.append_to_cflags "-Wno-implicit-function-declaration" if DevelopmentTools.clang_build_version >= 1200
   
       system "./configure", "--prefix=#{prefix}",
-                            "--enable-image",
-                            "--with-imagelib=imlib2",
+                            "--disable-image",
                             "--with-ssl=#{Formula["openssl@3"].opt_prefix}"
       system "make", "install"
     end
@@ -54,4 +65,3 @@ class W3m < Formula
       assert_match "DuckDuckGo", shell_output("#{bin}/w3m -dump https://duckduckgo.com")
     end
   end
-  
